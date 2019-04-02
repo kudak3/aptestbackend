831cce5b32359f3a1f9c4ab6c8e46e0d6485a618
truzane <truzane@hitrac.co.zw>
2019-04-02 16:12:19 +0200

    add security

--- src/main/java/com/aptest/Aptest/configuration/SecurityConfig.java
+++ src/main/java/com/aptest/Aptest/configuration/SecurityConfig.java
@@ -29,7 +29,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
             "**/swagger-resources/**",
             "/swagger-ui.html"
 
-            
+
     };
 
     @Override
